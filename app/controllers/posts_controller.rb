f48d61232c099908180df897ce423fbc7844cf28
root <rajeshnuthalapati499@gmail.com>
2017-12-05 07:40:50 +0000

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  
+
+
+  def index
+    @posts =Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+      end
+  end
+
+  def new
+   @post = Post.new
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+
+   private 
+    def post_params
+      params.require(:post).permit(:title,:body)
+    end
+
+    
+  end
