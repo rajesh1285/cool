e93fdd2571ee8e16dd27b0ec46c22f826fd5d0c7
root <root@localhost.localdomain>
2017-12-07 12:04:23 +0000

    add pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,23 @@
 class CommentsController < ApplicationController
 
 	 def create
-	 	@post =@post.find(params[:post_id])
-	 	@comment =@post.comments.create(params[:comment].permit(:name,:body))
+	 	@post = Post.find(params[:post_id])
+	 	@comment = @post.comments.create(comment_params)
 	 	redirect_to post_path(@post)
 
 	 end
 
-	 def update
+	 def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
 
-
+		redirect_to post_path(@post)
+	
 	 end
+
+	 private
+	  def comment_params
+       params.require(:comment).permit(:name , :body)
+	  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do  
   root "posts#index"
+  
   resources :posts do 
   	resources :comments
   end
+  #get 'pages/about'
+
+get '/about', :to => 'pages#about'  
 end
